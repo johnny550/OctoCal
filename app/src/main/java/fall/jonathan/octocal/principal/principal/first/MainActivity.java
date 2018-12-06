ffd7b54f26854e358caa023b1ff39626bc5feff8
jonathan <sergeijohnny@gmail.com>
2018-12-06 14:30:54 +0900

    Unit test for first package

--- app/src/main/java/fall/jonathan/octocal/principal/principal/first/MainActivity.java
+++ app/src/main/java/fall/jonathan/octocal/principal/principal/first/MainActivity.java
@@ -2,6 +2,7 @@ package fall.jonathan.octocal.principal.principal.first;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import fall.jonathan.octocal.R;
 
@@ -19,5 +20,19 @@ public class MainActivity extends AppCompatActivity implements UnderinningInterf
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myPres = new Presenter(this);
+    }
+
+
+    //View methods
+    @Override
+    public void signInScreen() {
+        Toast toast=Toast.makeText(this, "Taking you to the sign in page", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void signUpScreen() {
+        Toast toast=Toast.makeText(this, "Taking you to the sign up page",Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
